ab3de757d62c07d1c717a21d802dee0c12ab3b81
Jack Dai <jdai88@gmail.com>
2026-08-27 06:50:19 -0700

    Copy dotfiles into place instead of symlinking (#2)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-# Sets up environment and symlinks
+# Sets up environment and copies config files into place
+# Run from the home directory
+
+DOTFILES_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# Copies a file/directory from the dotfiles repo, leaving any existing file alone
+install_file() {
+    src=$DOTFILES_DIR/$1
+    dest=$2
+    if [ -e "$dest" ]; then
+        echo "Skipping $dest, already exists"
+    else
+        cp -r "$src" "$dest"
+    fi
+}
 
 # Clone vim plugins into .vim/pack/jack/start
 [ -z "$VIM_PACK_DIR" ] && VIM_PACK_DIR=.vim/pack/jack
@@ -8,16 +22,16 @@ git -C $VIM_PACK_DIR/start clone git@github.com:wellle/targets.vim.git
 git -C $VIM_PACK_DIR/start clone git@github.com:tpope/vim-commentary.git
 git -C $VIM_PACK_DIR/start clone git@github.com:altercation/vim-colors-solarized.git
 
-[ ! -e .vimrc ] && ln -s dotfiles/vim/vimrc .vimrc
+install_file vim/vimrc .vimrc
 mkdir -p .vim/backup
 mkdir -p .vim/undo
 
-[ ! -e .gitconfig ] && ln -s dotfiles/git/gitconfig .gitconfig
-[ ! -e .gitignore_global ] && ln -s dotfiles/git/gitignore_global .gitignore_global
+install_file git/gitconfig .gitconfig
+install_file git/gitignore_global .gitignore_global
 
-[ ! -e .terminfo ] && ln -s dotfiles/terminfo .terminfo
+install_file terminfo .terminfo
 
-[ ! -e .zshrc ] && ln -s dotfiles/shell/zshrc .zshrc
-[ ! -e .aliases ] && ln -s dotfiles/shell/aliases .aliases
+install_file shell/zshrc .zshrc
+install_file shell/aliases .aliases
 
-[ ! -e .tmux.conf ] && ln -s dotfiles/tmux.conf .tmux.conf
+install_file tmux.conf .tmux.conf
